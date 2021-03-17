74bca3b2338320ea894d2eda9bc941bc5e0326d5
shantanu <shantanu@shantanus-MacBook-Pro.local>
2021-03-17 22:24:26 +0530

    added API URL

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAQFTpqCdHeAbImEPw_GZCLuUmvxJytdMI",
+    authDomain: "saidemo-523c7.firebaseapp.com",
+    databaseURL: "https://saidemo-523c7.firebaseio.com",
+    projectId: "saidemo-523c7",
+    storageBucket: "saidemo-523c7.appspot.com",
+    messagingSenderId: "855264868612",
+    appId: "1:855264868612:web:26d56a1390c3b8e140f851",
+    measurementId: "G-692MXXM9J3"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAQFTpqCdHeAbImEPw_GZCLuUmvxJytdMI",
+    authDomain: "saidemo-523c7.firebaseapp.com",
+    databaseURL: "https://saidemo-523c7.firebaseio.com",
+    projectId: "saidemo-523c7",
+    storageBucket: "saidemo-523c7.appspot.com",
+    messagingSenderId: "855264868612",
+    appId: "1:855264868612:web:26d56a1390c3b8e140f851",
+    measurementId: "G-692MXXM9J3"
+  }
+};
